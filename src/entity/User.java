05c19e4db76dd5f002903bfe77bb18699cae674d
anastasiia <Anastasiia.Lepova@ivkhk.ee>
2021-10-14 13:38:51 +0300

    добавили класс User и History

---
+++ src/entity/User.java
@@ -0,0 +1,35 @@
+
+package entity;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String tel;
+    
+    public User(){
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}
--- src/myclasses/App.java
+++ src/myclasses/App.java
@@ -3,11 +3,15 @@ package myclasses;
 
 import entity.Author;
 import entity.Book;
+import entity.History;
+import entity.User;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 public class App {
     public void run(){
-        System.out.println("Hello");
+        //System.out.println("Hello");
         Book book=new Book();
         book.setBookName("Voina i mir");
         book.setReleaseYear(2010);
@@ -16,6 +20,10 @@ public class App {
         author.setFirstName("Lev");
         author.setLastName("Tolstoy");
         author.setBirthYear(1828);
+        User user= new User();
+        user.setFirstName("Andrei");
+        user.setLastName("Koni");
+        user.setTel("+37256843420");
         authors[0]= author;
         book.setAuthors(authors);
         System.out.printf("Создана книга: %s, автор: %s %s, год издания: %d%n",
@@ -23,6 +31,21 @@ public class App {
                 book.getAuthors()[0].getFirstName(),
                 book.getAuthors()[0].getLastName(),
                 book.getReleaseYear()
+                );
+        System.out.printf("Имя пользователя: %s , фамилия: %s , номер телефона: %s%n",
+                user.getFirstName(),
+                user.getLastName(),
+                user.getTel()
+                );
+        History history= new History();
+        history.setBook(book);
+        history.setUser(user);
+        Calendar c=new GregorianCalendar();
+        history.setGivenBook(c.getTime());
+        System.out.printf("Пользователь %s %s взял книгу \"%s\"%n",
+                history.getUser().getFirstName(),
+                history.getUser().getLastName(),
+                history.getBook().getBookName()
                 );
     }
 }
